084e7e8f4e62890fe34bfff63df1d4356156fc35
cross-hair-scalp <cross.hair.scalp@gmail.com>
2026-07-17 11:06:34 +0900

    ホーム画面名更新用にキャッシュを更新

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE = 'pretowa-v1';
+const CACHE = 'pretowa-v2';
 const FILES = ['/'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
